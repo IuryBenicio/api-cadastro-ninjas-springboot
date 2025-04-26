0ddbe7e234dcf0d5f5ee00c2990d5a6eff26a771
IuryBenicio <iury620@gmail.com>
2025-04-26 12:10:15 -0300

    feat lombock to make the constructors and all the data

--- src/main/java/benicio/iury/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/benicio/iury/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package benicio.iury.CadastroDeNinjas.Missoes;
 
 import benicio.iury.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -20,50 +26,8 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninjas;
 
-    //construtores
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String titulo, String descricao, DificuldadesMissoes dificuldade) {
-        this.id = id;
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.dificuldade = dificuldade;
-    }
-
-    //getters
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public DificuldadesMissoes getDificuldade() {
-        return dificuldade;
-    }
-
-    //setters
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public void setDificuldade(DificuldadesMissoes dificuldade) {
-        this.dificuldade = dificuldade;
+    @Override
+    public String toString() {
+        return "Missão com o nome de: " + this.titulo + ", descrição: " + this.descricao;
     }
 }
--- src/main/java/benicio/iury/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/benicio/iury/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,15 @@ package benicio.iury.CadastroDeNinjas.Ninjas;
 
 import benicio.iury.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity // transforma a classe em entidade
 @Table(name = "tb_Ninjas_Cadastrados")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -15,46 +19,9 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
-
     //um ninja pode ter apenas uma missão
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Foreing Key - chave estrangeira
     private MissoesModel missao;
 
-    //Constructors
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    //Getters
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    //Setters
-    public void setNome(String nome) {
-
-        this.nome = nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
